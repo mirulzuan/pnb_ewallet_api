af01104423cbdf3f25696ab899cfd58abd1b2eab
amirul.hisamuddin <mirulzuan67@gmail.com>
2019-08-13 01:01:08 +0800

    add jwt

--- lib/jason_web_token.rb
+++ lib/jason_web_token.rb
@@ -1,1 +1,13 @@
+class JsonWebToken
+  SALT = "this-too-shall-pass"
 
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SALT)
+  end
+
+  def self.decode(token)
+    decoded = JWT.decode(token, SALT)[0]
+    HashWithIndifferentAccess.new decoded
+  end
+end
